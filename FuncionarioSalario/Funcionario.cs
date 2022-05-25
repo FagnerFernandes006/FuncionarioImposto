027c5a343533410e7cfc5ed6bac8ea35c05052a2
Fagner Fernandes <Fagner Fernandes@DESKTOP-ACNLJLV>
2022-05-24 22:38:12 -0300

    FuncionarioImposto

---
+++ FuncionarioSalario/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+namespace FuncionarioSalario
+{
+    public class Funcionario
+    {
+        public string nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+
+       public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            double aumento = SalarioBruto * (porcentagem / 100);
+            SalarioBruto = aumento + SalarioLiquido();
+        }
+        public override string ToString()
+        {
+            return nome + ", " + "$ " + SalarioBruto.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ FuncionarioSalario/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+namespace FuncionarioSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + funcionario.ToString());
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine("Dados atualizados: " + funcionario.ToString());
+        }
+    }
+}
